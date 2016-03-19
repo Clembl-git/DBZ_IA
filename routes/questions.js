bf85c80307176bd5fccd3f94f5f33eb91bb71500
JulienCt <j.catteau34@gmail.com>
2016-03-19 19:20:19 +0100

    Utilisation de promise

--- routes/questions.js
+++ routes/questions.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
-var personnages = require('./personnages');
 
-var listQuestions =[];
-
-var listPersonnages = personnages.getList;
-console.log("Je suis le premier");
-console.log(personnages.getList);
+var listPersonnages;
+var listQuestions = [];
 
 var connection = mysql.connectToDB();
 connection.connect(function(err) {
@@ -15,50 +11,78 @@ connection.connect(function(err) {
     console.log("erreur de connection : "+err);
 })
 
-/*Getter Question*/
-router.get('/', function(req, res, next) {
-	var result = 'RESULT: ';
+//On récupère la liste des personnages
+require("../Controllers/personnageController.js").getList.then(function(listPerso, err)  {
+    listPersonnages = listPerso;
+    console.log(listPersonnages);
+});
+
+//On récupère la liste des questions
+getListQuestion();
+
+/*Methods*/
+function getListQuestion()
+{
 	connection.query("SELECT * FROM Question", function(err, rows){
 		if(err) throw err;
 		for(var i = 0; i < rows.length; i++)
   		{
   			if(rows[i].libelleQuestion != 'undefined')
         {
-  		    result += rows[i].libelleQuestion;
           listQuestions[i] = rows[i].idQuestion;
         }
   		}
-	  res.json(result);
 	});
-});
+}
 
-router.get('/getScoreQuestions', function(req, res, next) {
+function getScoreQuestion()
+{
   score = 0;
   var strIdQuestion;
-  for(var id in listQuestions)
+  var strIdPersonnage;
+
+  //On pépare la chaine avec les id des questions encore en lice
+  for(var idQuestion in listQuestions)
   {
-    strIdQuestion += id + ",";
+    strIdQuestion += idQuestion + ",";
+  }
+  //Idem pour les personnages
+  for(var idPersonnage in listPersonnages)
+  {
+    strIdPersonnage += idPersonnage + ",";
   }
 
-  for(var y = 1; y < 50; y++ ) 
+  console.log("ListQuestion : " + listQuestions);
+  console.log("Debut boucle" + listQuestions.length);
+  for(var y = 0; y < listQuestions.length; y++ )
   {
-    for(var i = 1; i < 5; i++ ) 
+    for(var i = 1; i < 5; i++ )
     {
-      connection.query("Select ( (nbPerso / nbChoix) + 1  ) as score FROM(  SELECT count(idPersonnage) as nbPerso FROM Personnage  ) as nbPerso ,(  SELECT count(*) as nbChoix  FROM Personnage P, Réponse R, Question Q  WHERE Q.idQuestion in ("+strIdQuestion+") and R.idPersonnage = P.idPersonnage AND R.idQuestion = Q.idQuestion AND R.idChoix = '"+i+"'    AND R.idQuestion = '"+y+"') as nbChoix", function(err, scoreQuestion){
-        for(var i = 0 ; i < scoreQuestion.length ; i++) 
-        {
-          if(scoreQuestion[i].score != null) 
+        connection.query("Select ( (nbPerso / nbChoix) + 1  ) as score FROM(  SELECT count(idPersonnage) as nbPerso FROM Personnage  ) as nbPerso ,(  SELECT count(*) as nbChoix  FROM Personnage P, Réponse R, Question Q  WHERE Q.idQuestion in ("+listQuestions+") and R.idPersonnage in ("+listPersonnages+") and R.idPersonnage = P.idPersonnage AND R.idQuestion = Q.idQuestion AND R.idChoix = '"+i+"'    AND R.idQuestion = '"+listQuestions[y]+"') as nbChoix", function(err, scoreQuestion){
+          if(err) throw err;
+          for(var i = 0 ; i < scoreQuestion.length ; i++)
           {
-            console.log("RETOUR SQL:");
-            console.log(scoreQuestion[i].score);
-            score += scoreQuestion[i].score;
+            if(scoreQuestion[i].score != null)
+            {
+              score += scoreQuestion[i].score;
+            }
           }
-        }
       });
     }
+    console.log("Score de la question "+listQuestions[y]+" : " + score);
   }
-    console.log("DONEDONE"+score);
-    res.json("Score : "+score);
+}
+
+/*Routes*/
+//Retourne la liste de toutes les questions en json
+router.get('/', function(req, res, next) {
+  res.json(listQuestions);
+});
+
+//Retourne le score de chaque questions pour le prochain tour
+router.get('/getScoreQuestions', function(req, res, next) {
+  getScoreQuestion();
+  res.json("NICE");
 });
 
 module.exports = router;
