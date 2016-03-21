0cc13e49bd7adeadac36847f4006db240cda1bd7
Clembl-git <clement.mab@gmail.com>
2016-03-21 22:52:01 +0100

    Reduction du nombre de requête récupérant le score des questions
    Reduction du nombre de requête récupérant le score des questions

--- routes/questions.js
+++ routes/questions.js
@@ -14,7 +14,7 @@ connection.connect(function(err) {
 //On récupère la liste des personnages
 require("../Controllers/personnageController.js").getList.then(function(listPerso, err)  {
     listPersonnages = listPerso;
-    console.log(listPersonnages);
+    console.log("question.js:getlist"+listPersonnages);
 });
 
 //On récupère la liste des questions
@@ -37,40 +37,66 @@ function getListQuestion()
 
 function getScoreQuestion()
 {
-  score = 0;
+  score = new Array();
   var strIdQuestion;
   var strIdPersonnage;
 
   //On pépare la chaine avec les id des questions encore en lice
   for(var idQuestion in listQuestions)
-  {
     strIdQuestion += idQuestion + ",";
-  }
   //Idem pour les personnages
   for(var idPersonnage in listPersonnages)
-  {
     strIdPersonnage += idPersonnage + ",";
-  }
 
   console.log("ListQuestion : " + listQuestions);
   console.log("Debut boucle" + listQuestions.length);
-  for(var y = 0; y < listQuestions.length; y++ )
+
+
+/*
+    NOUVELLE REQUËTE POUR ENLEVER LA BOUCLE FOR ET GROUP BY idQuestion et idChoix.
+    82 Résultat, à vérifier !
+
+  SELECT  needSum.idChoix, SUM(needSum.Score), needSum.idQuestion
+FROM (
+SELECT nbChoix.idChoix as idChoix, ( (nbPerso / nbChoix) + 1  ) as Score, nbChoix.idQuestion
+                 FROM (
+                  SELECT count(idPersonnage) as nbPerso FROM Personnage ) as nbPerso,
+                    ( SELECT R.idChoix as idChoix, count(*) as nbChoix, Q.idQuestion as idQuestion
+                        FROM Personnage P, Réponse R, Question Q
+                        WHERE
+                        R.idPersonnage = P.idPersonnage
+                        AND R.idQuestion = Q.idQuestion
+                        GROUP BY R.idChoix, Q.idQuestion
+                    ) as nbChoix
+                GROUP BY nbChoix.idQuestion, nbChoix.idChoix ) as needSum
+GROUP BY needSum.idChoix, needSum.idQuestion
+*/
+  for(var i = 0; i < listQuestions.length; i++ )
   {
-    for(var i = 1; i < 5; i++ )
-    {
-        connection.query("Select ( (nbPerso / nbChoix) + 1  ) as score FROM(  SELECT count(idPersonnage) as nbPerso FROM Personnage  ) as nbPerso ,(  SELECT count(*) as nbChoix  FROM Personnage P, Réponse R, Question Q  WHERE Q.idQuestion in ("+listQuestions+") and R.idPersonnage in ("+listPersonnages+") and R.idPersonnage = P.idPersonnage AND R.idQuestion = Q.idQuestion AND R.idChoix = '"+i+"'    AND R.idQuestion = '"+listQuestions[y]+"') as nbChoix", function(err, scoreQuestion){
-          if(err) throw err;
-          for(var i = 0 ; i < scoreQuestion.length ; i++)
-          {
-            if(scoreQuestion[i].score != null)
-            {
-              score += scoreQuestion[i].score;
-            }
-          }
-      });
-    }
-    console.log("Score de la question "+listQuestions[y]+" : " + score);
+    connection.query("SELECT nbChoix.idChoix, ( (nbPerso / nbChoix) + 1  ) as score, nbChoix.idQuestion " +
+                   "FROM (" +
+                    "SELECT count(idPersonnage) as nbPerso FROM Personnage ) as nbPerso, " +
+                      "( SELECT R.idChoix as idChoix, count(*) as nbChoix, Q.idQuestion " +
+                          "FROM Personnage P, Réponse R, Question Q " +
+                          "WHERE Q.idQuestion in ("+listQuestions+") and R.idPersonnage in ("+listPersonnages+") "+
+                          "AND R.idPersonnage = P.idPersonnage " +
+                          "AND R.idQuestion = Q.idQuestion " +
+                          "AND R.idQuestion = '"+listQuestions[i]+"' "+
+                          "GROUP BY R.idChoix, Q.idQuestion " +
+                      ") as nbChoix GROUP BY nbChoix.idQuestion", function(err, scoreQuestion){
+      if (err)  throw err;
+      console.log("retoursql : "+scoreQuestion);
+      for (var i = 0 ; i < scoreQuestion.length ; i++)
+      {
+        if (scoreQuestion[i].score != null) {
+          score.value = scoreQuestion[i].score;
+          score.choix = scoreQuestion[i].idChoix;
+          console.log("score :"+score.value);
+        }
+      }
+    });
   }
+  console.log("Score de la question "+listQuestions[i]+" : " + score);
 }
 
 /*Routes*/
@@ -81,7 +107,11 @@ router.get('/', function(req, res, next) {
 
 //Retourne le score de chaque questions pour le prochain tour
 router.get('/getScoreQuestions', function(req, res, next) {
-  getScoreQuestion();
+  require("../Controllers/personnageController.js").getList.then(function(listPerso, err)  {
+    listPersonnages = listPerso;
+    console.log("question.js:getlist"+listPersonnages);
+      getScoreQuestion();
+});
   res.json("NICE");
 });
 
