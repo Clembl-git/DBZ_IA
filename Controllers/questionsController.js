785b3fe6414bc26729f523f172698f82bab80944
JulienCt <j.catteau34@gmail.com>
2016-04-17 16:01:13 +0200

    Modification de la méthode getQuestionAPose()

--- Controllers/questionsController.js
+++ Controllers/questionsController.js
@@ -20,22 +20,56 @@ module.exports = {
         var deferred = Q.defer();
         mysql.connectToDB().then(function(connection) {
             var listQuestionAPose = new Array();
-            connection.query("SELECT Q.libelleQuestion, R.idChoix as idChoix, Q.idQuestion, (" + listPersonnagesRestant.length + "/count(*) + 1) as score " +
+            connection.query("SELECT Q.libelleQuestion, R.idChoix as idChoix, Q.idQuestion, (count(*) + 1) as score " +
                     "FROM Personnage P, Réponse R, Question Q " +
                     "WHERE  R.idPersonnage = P.idPersonnage " +
                     "AND R.idQuestion = Q.idQuestion " +
                     "AND Q.idQuestion not in (" + listeExclusionQuestion + ") and R.idPersonnage in (" + listPersonnagesRestant + ") " +
-                    "GROUP BY R.idChoix, Q.idQuestion " +
-                    "having score >= (select MAX(score) from (SELECT (" + listPersonnagesRestant.length + "/count(*) + 1) as score " +
-																															"FROM Personnage P, Réponse R, Question Q " +
-																															"WHERE  R.idPersonnage = P.idPersonnage " +
-																															"AND R.idQuestion = Q.idQuestion " +
-																															"AND Q.idQuestion not in (" + listeExclusionQuestion + ") and R.idPersonnage in (" + listPersonnagesRestant + ") " +
-																															"GROUP BY R.idChoix, Q.idQuestion) as f)  ")
+                    "GROUP BY R.idChoix, Q.idQuestion ")
                 .then(function(listQuestion) {
+                  //Liste qui contient l'id de la question avec son score total et son libellé
+                  var listCumulScoreQuestion = {};
+
+                  //Liste qui contient les id des questions qui ont le plus haut score
+                  var listIdQuestionMaxScore = [];
+                  var maxScore = 0;
+
+                  //Pour chaque questions on fait le cumul des scores (Q1.R1 * Q1.R2 * ... * Q1.R5)
+                  for (var i = 0; i < listQuestion.length; i++) {
+                    var idQuestion = listQuestion[i].idQuestion;
+                    var scoreQuestionCourante = listQuestion[i].score;
+                    //Si l'id de la question n'est pas une clé du tableau on l'ajoute avec son score et son libellé
+                    if(!(idQuestion in listCumulScoreQuestion))
+                    {
+                      listCumulScoreQuestion[idQuestion] = {"score": scoreQuestionCourante, "libelle": listQuestion[i].libelleQuestion};
+
+                      if (scoreQuestionCourante > maxScore) {
+                        maxScore = scoreQuestionCourante;
+                        listIdQuestionMaxScore = [idQuestion];
+                      } else if (scoreQuestionCourante == maxScore) {
+                        listIdQuestionMaxScore.push(idQuestion);
+                      }
+                    }
+                    //Sinon on multiplie son score par celui en cours de lecture
+                    else {
+                      listCumulScoreQuestion[idQuestion].score = listCumulScoreQuestion[idQuestion].score * listQuestion[i].score;
+
+                      if (listCumulScoreQuestion[idQuestion].score > maxScore) {
+                        maxScore = listCumulScoreQuestion[idQuestion].score;
+                        listIdQuestionMaxScore = [idQuestion];
+                      } else if (listCumulScoreQuestion[idQuestion].score == maxScore) {
+                        listIdQuestionMaxScore.push(idQuestion);
+                      }
+                    }
+                  }
+                  console.log(listIdQuestionMaxScore);
+                  console.log(listCumulScoreQuestion);
+
+
+
+
+
 										var random = Math.floor((Math.random() * listQuestion.length - 1) + 1);
-										console.log(random);
-										console.log(listQuestion);
                     listQuestionAPose["id"] = listQuestion[random].idQuestion;
                     listQuestionAPose["libelle"] = listQuestion[random].libelleQuestion;
                     deferred.resolve(listQuestionAPose);
